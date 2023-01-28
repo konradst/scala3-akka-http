95c2c4247d69601e1f525b0e105d195d88efaf72
Konrad <konrad.stobiecki@gmail.com>
2023-01-28 23:12:46 +0100

    one main class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,43 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+/*
+ * Copyright (C) 2020-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
 
-def msg = "I was compiled by Scala 3. :)"
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
+
+object HttpServerRoutingMinimal {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.executionContext
+
+    val route =
+      path("hello") {
+        get {
+          complete(
+            HttpEntity(
+              ContentTypes.`text/html(UTF-8)`,
+              "<h1>Say hello to akka-http</h1>" +
+                "I was compiled by Scala 3. :)"
+            )
+          )
+        }
+      }
+
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
+
+    println(
+      s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop..."
+    )
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
